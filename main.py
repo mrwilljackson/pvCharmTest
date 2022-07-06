4d0d431fd5dcbd82e4bbb1e4b409837d267cf9ab
Will Jackson <will@play.physio>
2022-07-06 16:47:22 +0100

    Initial Commit added remote git text

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+This is a test project to try out with git
+"""
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('This is test project in PyCharm using remote Git')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
