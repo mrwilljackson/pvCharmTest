a07129995a40e2c71fc43edb309be3597a378b08
Will Jackson <will@play.physio>
2022-07-06 17:05:15 +0100

    add .gitignore

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('This is test project in PyCharm using remote Git')
+    print_hi('This is test project in PyCharm using remote Git with gitignore')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
